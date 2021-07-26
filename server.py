7951183154fcc2b34eb3e78f9872e32c9397332f
stephen-jr <stephen24jnr@gmail.com>
2021-07-26 03:57:37 +0100

    added train function frontend

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
+import imp
 import os
 import json
+import dill
 import shlex
 import subprocess
 from flask_cors import CORS
 # import keras.backend as k
 from flask import Flask, request, render_template, Response, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pandas as pd
+from build import preprocess_text
 
 process = None
 model = None
+tokenizer = None
 # template_dir = os.path.abspath('web')
 
+def error(msg):
+    return jsonify({
+        'error': msg
+    })
 
 def event_stream(system_command, **kwargs):
     # k.clear_session()
@@ -64,16 +75,44 @@ def train():
     # return Response(event_stream('py build.py --train '), mimetype="text/event-stream")
 
 
-@app.route('/classify', methods=['GET'])
+@app.route('/classify', methods=['POST'])
 def classify():
-    global model
-    if model is None:
-        pass  # load model
+    if request.json:
+        global model, tokenizer
+        if model is None:
+            model = load_model('models/model_final')
+        if tokenizer is None:
+            with open('utils/tokenizer.pkl', 'rb') as f:
+                tokenizer = dill.load(f)
+        try:
+            _f = pd.DataFrame(request.json)
+            try:
+                for key in ["text", "reviewText"]:
+                    if key in _f.columns:
+                        _pre = _f[key].astype("str").apply(preprocess_text)
+                        _t_text = tokenizer.texts_to_sequences(_pre)
+                        _t = pad_sequences(_t_text, padding="post", maxlen=70)
+                        print("==========Predicting===========")
+                        _p = model.predict(_t)
+                        _f['PreprocessedText'] = _pre
+                        _f['PredictionScores'] = _p
+                        _f['Polarity'] = ['Positive' if x >= 0.5 else 'Negative' for x in _p]
+                        _f.to_csv("classification/insight.csv")
+                        _s = _f.sample(n=10)
+                        return Response({
+                            'sentences': _s["text"].to_list(),
+                            'predictions': _s["PredictionScores"].astype("float").apply(lambda x: round(x, 5)).to_list(),
+                            'polarity': _s["Polarity"].to_list(),
+                            'insight': _f["Polarity"].value_counts().to_list()
+                        })
+                return error("Ensure Uploaded file has a text or reviewText column")
+            except BaseException as e:
+                return error(str(e))
+        except BaseException as e:
+            error( str(e) )
+    else:
+        return error("Invalid request data")
+   
 
-    try:
-        pass  # run classification
-
-    except BaseException as e:
-        exit(e)
 
 app.run()
--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,189 +1,257 @@
 /* jshint esversion : 6 */
 
-var fileData;
+var fileData, ext, type = window.location.pathname.split('.')[0].split('/').pop();
 
-var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({loop: true})
-.add({
-  targets: '.ml12 .letter',
-  translateX: [40,0],
-  translateZ: 0,
-  opacity: [0,1],
-  easing: "easeOutExpo",
-  duration: 1200,
-  delay: (el, i) => 500 + 30 * i
-}).add({
-  targets: '.ml12 .letter',
-  translateX: [0,-30],
-  opacity: [1,0],
-  easing: "easeInExpo",
-  duration: 1100,
-  delay: (el, i) => 100 + 30 * i
-});
+var textWrapper = document.querySelector(".ml12");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml12 .letter",
+    translateX: [40, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i,
+  })
+  .add({
+    targets: ".ml12 .letter",
+    translateX: [0, -30],
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i,
+  });
 
 const hasExtension = () => {
-  let fileName = $('#upload').val();
-  return (new RegExp('(' + ['csv','json'].join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
+  let fileName = $("#upload").val();
+  return new RegExp(
+    "(" + ["csv", "json"].join("|").replace(/\./g, "\\.") + ")$"
+  ).test(fileName);
 };
 
 const notification = (opt) => {
-  if( !$.isEmptyObject(opt) ){
-    let cls ='info';
-    if( opt.type ) cls = opt.type;
+  if (!$.isEmptyObject(opt)) {
+    let cls = "info";
+    if (opt.type) cls = opt.type;
     let message = "Information";
-    if(opt.message){
-      message = opt.message
+    if (opt.message) {
+      message = opt.message;
     }
-    $('.notification-container').html(`<div class="wow animate__animated animate__fadeInRight alert alert-${cls}">
+    $(".notification-container")
+      .html(`<div class="wow animate__animated animate__fadeInRight alert alert-${cls}">
                                           <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                           <p>${message}</p>
                                       </div>`);
-    setTimeout(function(){
-      $('.notification-container').html(' ');
+    setTimeout(function () {
+      $(".notification-container").html(" ");
     }, 12000);
   }
-}
-
-const upload = () => {
-  if(!hasExtension()) notification({
-    message : 'Incorrect file. Use a csv or json file',
-    type: 'danger'
-  });
-  else{
-    $('#message').show();
-    $('#next').show();
-    $('.upld').hide();
-    $('.box').show();
-    data = event.target.files[0];
-    Papa.parse(data, {
-      header: true,
-      dynamicTyping: true,
-      complete: (results => {
-        console.log(results)
-        return false
-        let table_data = '<table class="table table-bordered table-striped col-md-12">'; 
-        table_data += "<tr>";
-        Object.keys(results.data[0]).forEach(k => { table_data +=`<th>${k}</th>`; });
-        table_data += "</tr>";
-        let table_slice = results.data.slice(0, 9);
-        table_slice.forEach(e=>{
-          table_data+='<tr>';
-          Object.values(e).forEach(v => { table_data +=`<td>${v}</td>`; });
-          table_data+='</tr>';
-        });
-        table_data += "</table>";
-        $("#file_table .tbl").html(table_data);
-        $('#insights_table').hide();
-        if($('#file_table').hasClass('col-md-7')){ 
-          $('#file_table').removeClass('col-md-7'); 
-        }
-        $('#table').show();
-        $('main .site__section').css('min-height', '40vh');
-        fileData = results;
-      })
-    });
-    }
 };
 
-const _beforeProcessDataUI  = () => {
-      $('main .site__section').css('min-height', '100vh');
-      $('#next').hide();
-      $('.box').hide();
-      $('#message').html('Please Wait while your file is processed');
-      $('#loader').show();
-      $('#table').hide();
-      return false
-}
+const formatTable = (results) => {
+  let table_data =
+  '<table class="table table-bordered table-striped col-md-12">';
+  table_data += "<tr>";
+  Object.keys(results.data[0]).forEach((k) => {
+    table_data += `<th>${k}</th>`;
+  });
+  table_data += "</tr>";
+  let table_slice = results.data.slice(0, 9);
+  table_slice.forEach((e) => {
+    table_data += "<tr>";
+    Object.values(e).forEach((v) => {
+      table_data += `<td>${v}</td>`;
+    });
+    table_data += "</tr>";
+  });
+  table_data += "</table>";
+  $("#file_table .tbl").html(table_data);
+  $("#insights_table").hide();
+  if ($("#file_table").hasClass("col-md-7")) {
+    $("#file_table").removeClass("col-md-7");
+  }
+  $("#table").show();
+  $("main .site__section").css("min-height", "40vh");
+  fileData = results;
+} 
 
-const _afterProcessDataUI = ( val ) => {
-    if(!$.isEmptyObject(val)){
-      if(val.error){
-         notification({
-            type: 'danger',
-            message: val.error
-          })
-         $('#loader').hide();
-         $('.upld').show();
-      }else{
-        $('#loader').hide();
-        $('.upld').show();
-        $('#next').hide();
-         notification({
-            type: 'success',
-            message: "Classification Successful. Find details in the classification folder"
-          })
-          table_data = `<table class="table table-bordered table-striped" style='margin:0 auto;'>`;
-          table_data += `<tr>
+const upload = () => {
+  if (!hasExtension())
+    notification({
+      message: "Incorrect file. Use a csv or json file",
+      type: "danger",
+    });
+  else {
+    $("#message").show();
+    $("#next").show();
+    $(".upld").hide();
+    $(".box").show();
+    data = event.target.files[0];
+    console.log(data);
+    ext = data.name.split(".").pop();
+    switch( ext ){
+      case 'csv':
+        Papa.parse(data, {
+          header: true,
+          dynamicTyping: true,
+          complete: (results) => {
+            formatTable(results);
+            return false;
+          },
+        });
+        break;
+      case 'json':
+        reader = new FileReader();
+        reader.readAsText(data)
+        reader.onload = (event) => {
+          // console.log(event.target.result);
+          formatTable({
+            'data':data
+          });
+        }
+        break;
+      default:
+        notification({
+          message: "Incorrect file. Use a csv or json file",
+          type: "danger",
+        });
+        break
+    }
+  }
+};
+
+const _beforeProcessDataUI = () => {
+  $("main .site__section").css("min-height", "100vh");
+  $("#next").hide();
+  $(".box").hide();
+  $("#message").html("Please Wait while your file is processed");
+  $("#loader").show();
+  $("#table").hide();
+  return false;
+};
+
+const _afterProcessDataUI = (val) => {
+  if (!$.isEmptyObject(val)) {
+    if (val.error) {
+      notification({
+        type: "danger",
+        message: val.error,
+      });
+      $("#loader").hide();
+      $(".upld").show();
+    } else {
+      $("#loader").hide();
+      $(".upld").show();
+      $("#next").hide();
+      notification({
+        type: "success",
+        message:
+          "Classification Successful. Find details in the classification folder",
+      });
+      table_data = `<table class="table table-bordered table-striped" style='margin:0 auto;'>`;
+      table_data += `<tr>
                           <th>Sentences</th><th>Prediction</th><th>Polarity</th>
                         </tr>`;
 
-          val.sentences.forEach((element, index) => {
-          table_data +=`<tr>
+      val.sentences.forEach((element, index) => {
+        table_data += `<tr>
                           <td>${element}</td>
                           <td>${val.predictions[index]}</td>
                           <td>${val.polarity[index]}</td>
                       </tr>`;
-          });
-          table_data += `</table>`;
+      });
+      table_data += `</table>`;
 
-          $('#message').html('Kindly a sample view of the result of the analysis');
-          $("#file_table .tbl").html(table_data).css('border-right', '1px solid #fff');
-          $('#file_table').removeClass('col-md-12').addClass('col-md-9');
+      $("#message").html("Kindly a sample view of the result of the analysis");
+      $("#file_table .tbl")
+        .html(table_data)
+        .css("border-right", "1px solid #fff");
+      $("#file_table").removeClass("col-md-12").addClass("col-md-9");
 
-          table_data = `<table class="table table-bordered table-striped">`;
-          Object.keys(val.insight).forEach(key => {
-            table_data+= `<tr>
+      table_data = `<table class="table table-bordered table-striped">`;
+      Object.keys(val.insight).forEach((key) => {
+        table_data += `<tr>
                               <td>${key.toString().toUpperCase()}</td>
-                              <td>${val.insight[ key ]}</td>
+                              <td>${val.insight[key]}</td>
                           </tr>`;
-          });
-          table_data+= '</table>';
-          $('#table').show();
-          $('#insights_table .tbl').html(table_data);$('#insights_table').show();
-          $('main .site__section').css('min-height', '40vh');
-      }
-    }else {
-      notification({
-        type: 'danger',
-        message: "Error get prediction data"
-      })
-       $('#loader').hide();
-       $('.upld').show();
+      });
+      table_data += "</table>";
+      $("#table").show();
+      $("#insights_table .tbl").html(table_data);
+      $("#insights_table").show();
+      $("main .site__section").css("min-height", "40vh");
     }
-    return false;
-}
+  } else {
+    notification({
+      type: "danger",
+      message: "Error get prediction data",
+    });
+    $("#loader").hide();
+    $(".upld").show();
+  }
+  return false;
+};
 
 const processData = () => {
+  _beforeProcessDataUI();
 
- _beforeProcessDataUI()
-
-  eel.classify(fileData.data)(val=> {
+  eel.classify(fileData.data)((val) => {
     console.log(val);
-        _afterProcessDataUI( val );
+    _afterProcessDataUI(val);
   });
 };
 
 const processDataFlask = () => {
-    _beforeProcessDataUI();
-    let source = new SSE('/train',{
-       headers:{
-        'Content-Type': 'application/json',
-       },
-       payload: JSON.stringify(fileData.data);
-    });
-}
+  _beforeProcessDataUI();
+  switch(type){
+    case 'upload':
+        $.ajax({
+          url: '/classify',
+          method: 'POST',
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify(fileData.data),
+          success: (val, status, xhr) =>{
+            _afterProcessDataUI(val);
+          },
+          error: (xhr, status, error) =>{
+            notification({
+              type: 'danger',
+              message: error
+            });
+          }
+        })
+      break;
+    case 'train':
+      let source = new SSE("/train", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        payload: JSON.stringify(fileData.data),
+      });
+      source.onmessage = (e) =>{
+        
+      }
+      break;
+  }
+};
 
 $(function () {
-  $('#upload').on('change', upload);
-  $('#next').hide();
-  $('.box').hide();
-  $('#loader').hide();
-  $('#message').hide();
-  $('#table').hide();
-//  $('#next').bind('click', processData);
-  $('#next').bind('click', processDataFlask);
-//  $('#nextTrain').bind('click', processDataFlask);
+  $("#upload").on("change", upload);
+  $("#next").hide();
+  $(".box").hide();
+  $("#loader").hide();
+  $("#message").hide();
+  $("#table").hide();
+  $('#console').hide();
+  //  $('#next').bind('click', processData);
+  $("#next").bind("click", processDataFlask);
+  //  $('#nextTrain').bind('click', processDataFlask);
 
   new WOW().init();
 });
